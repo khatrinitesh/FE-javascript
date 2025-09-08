f7a236c98644e3bfd7a3f1225ec09fe83f1055b5
nitesh <nitesh.khatri88@gmail.com>
2025-09-08 16:52:29 +0530

    updated

--- script.js
+++ script.js
@@ -138,3 +138,358 @@
 // }
 // const otp = generateOTP();
 // console.log("Your OTP is:", otp);
+
+// fetch("https://retoolapi.dev/njKKYt/data")
+//   .then((response) => {
+//     if (!response.ok) {
+//       throw new Error(`HTTP error! Status: ${response.status}`);
+//     }
+//     return response.json();
+//   })
+//   .then((data) => {
+//     console.log("Fetched data (Promise):", data);
+//   })
+//   .catch((error) => {
+//     console.error("Fetch error:", error);
+//   });
+
+// fetch("https://retoolapi.dev/njKKYt/data")
+//   .then((response) => {
+//     if (!response.ok) {
+//       throw new Error(`sorry something went wrong ${response.status}`);
+//     }
+//     return response.json();
+//   })
+//   .then((data) => {
+//     console.log(data);
+//   })
+//   .catch((error) => {
+//     console.error("fetch error", error);
+//   });
+
+// fetch("https://retoolapi.dev/njKKYt/data")
+//   .then((res) => res.json())
+//   .then((data) => console.log(data))
+//   .catch((error) => console.error("get error:", error));
+
+// async function getUserData() {
+//   const response = await fetch("https://retoolapi.dev/njKKYt/data");
+//   const data = await response.json();
+//   console.log(data);
+// }
+// getUserData();
+
+// const newItem = {
+//   name: "Nitesh",
+//   age: 25,
+// };
+
+// fetch("https://retoolapi.dev/njKKYt/data", {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+//   body: JSON.stringify(newItem),
+// })
+//   .then((res) => res.json())
+//   .then((data) => console.log("POST", data))
+//   .catch((error) => console.error("POST error", error));
+
+// const newItem = {
+//   Name: "sameet",
+//   age: 43,
+// };
+
+// fetch("https://retoolapi.dev/njKKYt/data", {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+//   body: JSON.stringify(newItem),
+// })
+//   .then((res) => res.json())
+//   .then((data) => console.log("POST DATA", data))
+//   .catch((error) => console.error("POST ERROR", error));
+
+// const updatedItem = {
+//   Name: "Sameet updated",
+//   age: 43,
+// };
+
+// fetch("https://retoolapi.dev/njKKYt/data/1", {
+//   method: "PUT",
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+//   body: JSON.stringify(updatedItem),
+// })
+//   .then((res) => res.json())
+//   .then((data) => console.log("PUT DATA", data))
+//   .catch((error) => console.error("ERROR DATA", error));
+
+// const newItem2 = {
+//   Name: "Arvind Khatri",
+//   age: 69,
+// };
+
+// fetch("https://retoolapi.dev/njKKYt/data", {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+//   body: JSON.stringify(newItem2),
+// })
+//   .then((res) => res.json())
+//   .then((data) => console.log("post data", data))
+//   .catch((error) => console.error("error data", error));
+
+// const updatedItem2 = {
+//   Name: "Arvind Khatri Updated",
+//   age: 67,
+// };
+
+// fetch("https://retoolapi.dev/njKKYt/data/67", {
+//   method: "PUT",
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+//   body: JSON.stringify(updatedItem2),
+// })
+//   .then((res) => res.json())
+//   .then((data) => console.log("put data", data))
+//   .catch((error) => console.error("fetch error", error));
+
+// fetch("https://retoolapi.dev/njKKYt/data/67", {
+//   method: "PUT",
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+//   body: JSON.stringify(updatedItem2),
+// })
+//   .then((res) => res.json())
+//   .then((data) => console.log("put data", data))
+//   .catch((error) => console.error("fetch error", error));
+
+// fetch("https://retoolapi.dev/njKKYt/data/67", {
+//   method: "PUT",
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+//   body: JSON.stringify(updatedItem2),
+// })
+//   .then((res) => res.json())
+//   .then((data) => console.log("put data", data))
+//   .catch((error) => console.error("fetch error", error));
+
+// fetch("https://retoolapi.dev/njKKYt/data/67", {
+//   method: "DELETE",
+// })
+//   .then((res) => {
+//     if (!res.ok) {
+//       console.log("DELETE: Item deleted successfully");
+//     } else {
+//       console.error("DELETE failed");
+//     }
+//   })
+//   .catch((error) => console.error("fetch error", error));
+
+// fetch("https://retoolapi.dev/njKKYt/data/4", {
+//   method: "DELETE",
+// })
+//   .then((res) => {
+//     if (!res.ok) {
+//       console.log("DELETE: Item deleted successfully");
+//     } else {
+//       console.log("DELETE failed");
+//     }
+//   })
+//   .catch((error) => console.error("fetch error", error));
+
+// get
+// fetch("https://jsonplaceholder.typicode.com/posts")
+//   .then((response) => response.json())
+//   .then((data) => console.log(data))
+//   .catch((error) => console.error("fetch error", error));
+
+// const updatedItemTypicodeModify = {
+//   userId: 101,
+//   id: 202,
+//   title: "nitesh khatri",
+//   body: "modify - nitesh is feeling awesomely",
+// };
+
+// PUT request
+// fetch("https://jsonplaceholder.typicode.com/posts/202", {
+//   method: "PUT",
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+//   body: JSON.stringify(updatedItemTypicodeModify),
+// })
+//   .then((response) => {
+//     if (!response.ok) {
+//       throw new Error(`PUT failed: ${response.status}`);
+//     }
+//     return response.json();
+//   })
+//   .then((data) => console.log("PUT MODIFY DATA:", data))
+//   .catch((error) => console.error("PUT fetch error:", error));
+
+// POST request
+// fetch("https://jsonplaceholder.typicode.com/posts", {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+//   body: JSON.stringify(updatedItemTypicodeModify),
+// })
+//   .then((response) => {
+//     if (!response.ok) {
+//       throw new Error(`POST failed: ${response.status}`);
+//     }
+//     return response.json();
+//   })
+//   .then((data) => console.log("POST DATA:", data))
+//   .catch((error) => console.error("POST fetch error:", error));
+
+// async function updatePost() {
+//   try {
+//     const response = await fetch(
+//       "https://jsonplaceholder.typicode.com/posts/202",
+//       {
+//         method: "PUT",
+//         headers: {
+//           "Content-Type": "application/json",
+//         },
+//         body: JSON.stringify(updatedItemTypicodeModify),
+//       }
+//     );
+//     if (!response.ok) {
+//       throw new Error(`Put failed,${response.status}`);
+//     }
+//     const data = await response.json();
+//     console.log("put data modified", data);
+//   } catch (error) {
+//     console.error(`Error`, error);
+//   }
+// }
+
+// fetch("https://jsonplaceholder.typicode.com/posts")
+//   .then((res) => res.json())
+//   .then((data) => console.log("fetch data 1.51 pm", data))
+//   .catch((error) => console.error(`fetch error`, error));
+
+// fetch("https://jsonplaceholder.typicode.com/posts/1")
+//   .then((res) => res.json())
+//   .then((data) => console.log("fetch single data", data))
+//   .catch((error) => console.error(`fetch error`, error));
+
+// fetch("https://jsonplaceholder.typicode.com/posts", {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+//   body: {
+//     title: "foo",
+//     body: "bar",
+//     userId: 1,
+//   },
+// })
+//   .then((res) => res.json())
+//   .then((data) => console.log("added post data", data))
+//   .catch((error) => console.error(`Fetch error`, error));
+
+// fetch("https://fakestoreapi.com/products")
+//   .then((res) => res.json())
+//   .then((data) => console.log(data));
+
+// fetch("https://jsonplaceholder.typicode.com/posts", {
+//   method: "POST",
+//   headers: { "Content-Type": "application/json; charset=UTF-8" },
+//   body: JSON.stringify({
+//     title: "foo",
+//     body: "bar",
+//     userId: 1,
+//   }),
+// })
+//   .then((res) => res.json())
+//   .then((json) => console.log("POST response:", json))
+//   .catch((err) => console.error("POST error:", err));
+
+// fetch("https://jsonplaceholder.typicode.com/posts/1", {
+//   method: "PUT",
+//   headers: {
+//     "Content-Type": "application/json;charset=UTF-8",
+//   },
+//   body: JSON.stringify({
+//     id: 1,
+//     title: "foo",
+//     body: "bar",
+//     userId: 1,
+//   }),
+// })
+//   .then((res) => res.json())
+//   .then((data) => console.log("PUT response", data))
+//   .catch((error) => console.error("fetch error", error));
+
+// fetch("https://jsonplaceholder.typicode.com/posts/1", {
+//   method: "DELETE",
+// })
+//   .then((res) => {
+//     if (res.ok) {
+//       console.log("DELETE successful");
+//     } else {
+//       console.log("DELETE failed");
+//     }
+//   })
+//   .catch((error) => console.error(`DELETE error`, error));
+
+// const API = "https://jsonplaceholder.typicode.com/posts";
+// fetch(API)
+//   .then((res) => {
+//     if (!res.ok) {
+//       throw new Error(`Sorry something went wrong, ${res.status}`);
+//     }
+//     return res.json();
+//   })
+//   .then((data) => console.log("get all posts:", data))
+//   .catch((error) => console.error(error));
+
+// const API = "https://jsonplaceholder.typicode.com/posts/10";
+// fetch(API)
+//   .then((res) => {
+//     if (!res.ok) {
+//       throw new Error(`Sorry something went wrong, ${res.status}`);
+//     }
+//     return res.json();
+//   })
+//   .then((data) => console.log("get single posts:", data))
+//   .catch((error) => console.error(error));
+
+// fetch("https://dummyjson.com/auth/login", {
+//   method: "POST",
+//   headers: { "Content-Type": "application/json" },
+//   body: JSON.stringify({
+//     username: "emilys",
+//     password: "emilyspass",
+//     expiresInMins: 30, // optional, defaults to 60
+//   }),
+//   credentials: "include", // Include cookies (e.g., accessToken) in the request
+// })
+//   .then((res) => res.json())
+//   .then(console.log);
+
+// fetch("https://dummyjson.com/auth/login", {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+//   body: JSON.stringify({
+//     username: "emilys",
+//     password: "emilyspass",
+//     expiresInMins: 30, // optional, defaults to 60
+//   }),
+//   credentials: "include",
+// })
+//   .then((res) => res.json)
+//   .then(console.log);
