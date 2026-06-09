6050a8cfd55888d5e99051bcd36373f3c3c89e74
nitesh <nitesh.khatri88@gmail.com>
2026-06-09 15:23:35 +0530

    updated

---
+++ FE-javascript-9/FE-javascript-9.js
@@ -0,0 +1,120 @@
+// console.log(Boolean(1));
+// console.log(Boolean(0));
+// console.log(Boolean("hello"));
+// console.log(Boolean(""));
+
+// let isActive = true;
+// console.log(isActive.toString());
+// console.log(isActive.valueOf());
+
+// let x = 5;
+// console.log(x == 8);
+// console.log(x != 8);
+// console.log(x > 8);
+// console.log(x < 8);
+// console.log(x >= 5);
+// console.log(x <= 4);
+
+// let age = 20;
+// if (age >= 18) {
+//   console.log("Adult");
+// } else {
+//   console.log("Minor");
+// }
+
+// function isEven(num) {
+//   if (num % 2 == 0) {
+//     return true;
+//   }
+//   return false;
+// }
+// console.log(isEven(10));
+
+// function isEven(num) {
+//   return num % 2 === 0;
+// }
+// console.log(isEven(12));
+// console.log(isEven(15));
+
+// for (let i = 1; i < 50; i++) {
+//   console.log(`Index:${i}x${i}=${i * i}`);
+// }
+
+// let i = 0;
+// while (i < 16) {
+//   console.log(i);
+//   i++;
+// }
+
+// let numbers = [10, 20, 30];
+// for (let num of numbers) {
+//   console.log("numberwise:", num);
+// }
+
+// let person = {
+//   name: "Ashi",
+//   age: 30,
+// };
+// for (let key in person) {
+//   console.log(key, person[key]);
+// }
+
+// everything else is falsy
+// console.log(Boolean(0));
+// console.log(Boolean(""));
+// console.log(Boolean(null));
+// console.log(Boolean());
+// console.log(Boolean(undefined));
+// console.log(Boolean(NaN));
+
+// everything else is truthy
+// console.log(Boolean(1));
+// console.log(Boolean("hello"));
+// console.log(Boolean([]));
+// console.log(Boolean({}));
+// console.log(Boolean(-1));
+
+// let age = 25;
+// let hasID = true;
+// if (age >= 18 && hasID) {
+//   console.log("Allowed");
+// }
+
+// let isAdmin = false;
+// let isOwner = true;
+// if (isAdmin || isOwner) {
+//   console.log("access granted");
+// }
+
+// let isLoggedIn = false;
+// console.log(!isLoggedIn);
+
+// check if number is positive - boolean in dsa problems
+// function isPositive(num) {
+//   return num > 0;
+// }
+// console.log(isPositive(1));
+// console.log(isPositive(-111));
+
+// check if number is odd
+// function isOdd(num) {
+//   return num % 2 !== 0;
+// }
+// console.log(isOdd(45));
+// console.log(isOdd(44));
+
+// check if array is empty
+// function isEmptyArray(arr) {
+//   return arr.length === 0;
+// }
+// console.log(isEmptyArray([]));
+// console.log(isEmptyArray([1, 2]));
+
+// check if value exists in array
+// function containsValue(arr, target) {
+//   return arr.includes(target);
+// }
+// console.log(containsValue([1, 2, 3], 2, 222));
+// console.log(containsValue([1, 2, 3], 55, 66));
+
+
