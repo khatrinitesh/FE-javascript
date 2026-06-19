e0c8b3c90cc95f4f0e1eb2756bdea3f55a67ae1a
nitesh <nitesh.khatri88@gmail.com>
2026-06-19 17:47:54 +0530

    updated

---
+++ FE-javascript-10/FE-javascript-10.js
@@ -0,0 +1,6 @@
+console.log(isNumeric("123"));
+console.log(isNumeric("12.5"));
+console.log(isNumeric("-45"));
+console.log(isNumeric("abc"));
+console.log(isNumeric(""));
+console.log(isNumeric("12abc"));
---
+++ FE-javascript-11/FE-javascript-11.js
@@ -0,0 +1,19 @@
+// map - transform array
+// const arr = [1, 2, 3, 4];
+// const result = arr.map((val) => val * 2);
+// console.log(result);
+// const arr = [1, 2, 3, 4];
+// const result = [];
+// for (let i = 0; i < arr.length; i++) {
+//   result.push(arr[i] * 2);
+// }
+
+// filter - select elements find even numbers
+// const arr = [1, 2, 3, 4, 5, 6];
+// const even = arr.filter((val) => val % 2 === 0);
+// console.log("even number", even);
+
+// reduce - sum array find total sum
+// const arr = [1, 2, 3, 4, 5];
+// const sum_reduce = arr.reduce((acc, cul) => acc + cul, 0);
+// console.log(sum_reduce);
