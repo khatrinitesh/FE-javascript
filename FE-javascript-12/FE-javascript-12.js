5d1ab4eee6d151d0bcc61761f6aa47fab84f7b4f
nitesh <nitesh.khatri88@gmail.com>
2026-06-22 18:09:56 +0530

    updated

---
+++ FE-javascript-12/FE-javascript-12.js
@@ -0,0 +1,46 @@
+// const fruits = ["apple", "banana", "mango"];
+
+// const entries = fruits.entries();
+// console.log(entries.next().value);
+// console.log(entries.next().value);
+// console.log(entries.next().value);
+// console.log(entries.next().value);
+// console.log(entries.next().value);
+
+const fruits = ["apple", "banana", "mango"];
+// for (const [index, value] of fruits.entries()) {
+//   console.log(index, value);
+// }
+// const numbers = [10, 20, 30];
+// const result = numbers.every((num) => num * 2);
+// console.log(result);
+
+// const users = [
+//   { name: "Ashi", active: true },
+//   { name: "Rahul", active: true },
+// ];
+// const format = JSON.stringify(users);
+// console.log(JSON.parse(format));
+
+// const arr = [1, 2, 3, 4];
+// console.log(arr);
+
+const arr = [1, 2, 3, 4, 5];
+// console.log(arr);
+
+// console.log(arr.fill(111));
+
+// const n = 5;
+// const visited = new Array(n).fill(false);
+// console.log(visited);
+
+// const freq = new Array(26).fill(0);
+// const str = "apple";
+// for (let char of str) {
+//   const index = char.charCodeAt(0) - "a".charCodeAt(0);
+//   freq[index]++;
+// }
+
+// const matrix = new Array(3).fill(new Array(3).fill(0));
+// matrix[0][0] = 1;
+// console.log(matrix);
